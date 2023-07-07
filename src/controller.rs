443e751ebf870eb016fc305fe81045f189956c7c
Jesse Luehrs <doy@materialize.com>
2023-07-07 17:10:14 -0400

    cleanup and docs

--- src/controller.rs
+++ src/controller.rs
@@ -13,25 +13,265 @@ use kube_runtime::finalizer::{finalizer, Event};
 use rand::{thread_rng, Rng};
 use tracing::{event, Level};
 
-#[async_trait::async_trait]
+/// The [`Controller`] watches a set of resources, calling methods on the
+/// provided [`Context`] when events occur.
+pub struct Controller<Ctx: Context>
+where
+    Ctx: Send + Sync + 'static,
+    Ctx::Error: Send + Sync + 'static,
+    Ctx::Resource: Send + Sync + 'static,
+    Ctx::Resource: Clone + std::fmt::Debug + serde::Serialize,
+    for<'de> Ctx::Resource: serde::Deserialize<'de>,
+    <Ctx::Resource as Resource>::DynamicType:
+        Eq + Clone + std::hash::Hash + std::default::Default + std::fmt::Debug + std::marker::Unpin,
+{
+    client: kube::Client,
+    make_api: Box<dyn Fn(&Ctx::Resource) -> Api<Ctx::Resource> + Sync + Send + 'static>,
+    controller: kube_runtime::controller::Controller<Ctx::Resource>,
+    context: Ctx,
+}
+
+impl<Ctx: Context> Controller<Ctx>
+where
+    Ctx: Send + Sync + 'static,
+    Ctx::Error: Send + Sync + 'static,
+    Ctx::Resource: Send + Sync + 'static,
+    Ctx::Resource: Clone + std::fmt::Debug + serde::Serialize,
+    for<'de> Ctx::Resource: serde::Deserialize<'de>,
+    <Ctx::Resource as Resource>::DynamicType:
+        Eq + Clone + std::hash::Hash + std::default::Default + std::fmt::Debug + std::marker::Unpin,
+{
+    /// Creates a new controller for a namespaced resource using the given
+    /// `client`. The `context` given determines the type of resource
+    /// to watch (via the [`Context::Resource`] type provided as part of
+    /// the trait implementation). The resources to be watched will be
+    /// limited to resources in the given `namespace`. A [`ListParams`]
+    /// can be given to limit the resources watched (for instance,
+    /// `ListParams::default().labels("app=myapp")`).
+    pub fn namespaced(client: Client, context: Ctx, namespace: &str, lp: ListParams) -> Self
+    where
+        Ctx::Resource: Resource<Scope = NamespaceResourceScope>,
+    {
+        let make_api = {
+            let client = client.clone();
+            Box::new(move |resource: &Ctx::Resource| {
+                Api::<Ctx::Resource>::namespaced(client.clone(), &resource.namespace().unwrap())
+            })
+        };
+        let controller = kube_runtime::controller::Controller::new(
+            Api::<Ctx::Resource>::namespaced(client.clone(), namespace),
+            lp,
+        );
+        Self {
+            client,
+            make_api,
+            controller,
+            context,
+        }
+    }
+
+    /// Creates a new controller for a namespaced resource using the given
+    /// `client`. The `context` given determines the type of resource to
+    /// watch (via the [`Context::Resource`] type provided as part of the
+    /// trait implementation). The resources to be watched will not be
+    /// limited by namespace. A [`ListParams`] can be given to limit the
+    /// resources watched (for instance,
+    /// `ListParams::default().labels("app=myapp")`).
+    pub fn namespaced_all(client: Client, context: Ctx, lp: ListParams) -> Self
+    where
+        Ctx::Resource: Resource<Scope = NamespaceResourceScope>,
+    {
+        let make_api = {
+            let client = client.clone();
+            Box::new(move |resource: &Ctx::Resource| {
+                Api::<Ctx::Resource>::namespaced(client.clone(), &resource.namespace().unwrap())
+            })
+        };
+        let controller = kube_runtime::controller::Controller::new(
+            Api::<Ctx::Resource>::all(client.clone()),
+            lp,
+        );
+        Self {
+            client,
+            make_api,
+            controller,
+            context,
+        }
+    }
+
+    /// Creates a new controller for a cluster-scoped resource using the
+    /// given `client`. The `context` given determines the type of resource
+    /// to watch (via the [`Context::Resource`] type provided as part of the
+    /// trait implementation). A [`ListParams`] can be given to limit the
+    /// resources watched (for instance,
+    /// `ListParams::default().labels("app=myapp")`).
+    pub fn cluster(client: Client, context: Ctx, lp: ListParams) -> Self
+    where
+        Ctx::Resource: Resource<Scope = ClusterResourceScope>,
+    {
+        let make_api = {
+            let client = client.clone();
+            Box::new(move |_: &Ctx::Resource| Api::<Ctx::Resource>::all(client.clone()))
+        };
+        let controller = kube_runtime::controller::Controller::new(
+            Api::<Ctx::Resource>::all(client.clone()),
+            lp,
+        );
+        Self {
+            client,
+            make_api,
+            controller,
+            context,
+        }
+    }
+
+    /// Run the controller. This method will not return. The [`Context`]
+    /// given to the constructor will have its [`apply`](Context::apply)
+    /// method called when a resource is created or updated, and its
+    /// [`cleanup`](Context::cleanup) method called when a resource is about
+    /// to be deleted.
+    pub async fn run(self) {
+        let Self {
+            client,
+            make_api,
+            controller,
+            context,
+        } = self;
+        let backoffs = Arc::new(Mutex::new(BTreeMap::new()));
+        let backoffs = &backoffs;
+        controller
+            .run(
+                |resource, context| {
+                    let uid = resource.uid().unwrap();
+                    let backoffs = Arc::clone(backoffs);
+                    context
+                        ._reconcile(client.clone(), make_api(&resource), resource)
+                        .inspect(move |result| {
+                            if result.is_ok() {
+                                backoffs.lock().unwrap().remove(&uid);
+                            }
+                        })
+                },
+                |resource, err, context| {
+                    let consecutive_errors = {
+                        let uid = resource.uid().unwrap();
+                        let mut backoffs = backoffs.lock().unwrap();
+                        let consecutive_errors: u32 =
+                            backoffs.get(&uid).copied().unwrap_or_default();
+                        backoffs.insert(uid, consecutive_errors.saturating_add(1));
+                        consecutive_errors
+                    };
+                    context.error_action(resource, err, consecutive_errors)
+                },
+                Arc::new(context),
+            )
+            .for_each(|reconciliation_result| async move {
+                let dynamic_type = Default::default();
+                let kind = Ctx::Resource::kind(&dynamic_type).into_owned();
+                match reconciliation_result {
+                    Ok(resource) => {
+                        event!(
+                            Level::INFO,
+                            resource_name = %resource.0.name,
+                            controller = Ctx::FINALIZER_NAME,
+                            "{} reconciliation successful.",
+                            kind
+                        );
+                    }
+                    Err(err) => event!(
+                        Level::ERROR,
+                        err = %err,
+                        source = err.source(),
+                        controller = Ctx::FINALIZER_NAME,
+                        "{} reconciliation error.",
+                        kind
+                    ),
+                }
+            })
+            .await
+    }
+}
+
+/// The [`Context`] trait should be implemented in order to provide callbacks
+/// for events that happen to resources watched by a [`Controller`].
+#[cfg_attr(docs_rs, feature(async_fn_in_trait))]
+#[cfg_attr(not(docs_rs), async_trait::async_trait)]
 pub trait Context {
+    /// The type of Kubernetes [resource](Resource) that will be watched by
+    /// the [`Controller`] this context is passed to
     type Resource: Resource;
+    /// The error type which will be returned by the [`apply`](Self::apply)
+    /// and [`cleanup`](Self::apply) methods
     type Error: std::error::Error;
 
+    /// The name to use for the finalizer. This must be unique across
+    /// controllers - if multiple controllers with the same finalizer name
+    /// run against the same resource, unexpected behavior can occur.
     const FINALIZER_NAME: &'static str;
 
+    /// This method is called when a watched resource is created or updated.
+    /// The [`Client`] used by the controller is passed in to allow making
+    /// additional API requests, as is the resource which triggered this
+    /// event. If this method returns `Some(action)`, the given action will
+    /// be performed, otherwise if `None` is returned,
+    /// [`success_action`](Self::success_action) will be called to find the
+    /// action to perform.
     async fn apply(
         &self,
         client: Client,
         resource: &Self::Resource,
     ) -> Result<Option<Action>, Self::Error>;
+
+    /// This method is called when a watched resource is marked for deletion.
+    /// The [`Client`] used by the controller is passed in to allow making
+    /// additional API requests, as is the resource which triggered this
+    /// event. If this method returns `Some(action)`, the given action will
+    /// be performed, otherwise if `None` is returned,
+    /// [`success_action`](Self::success_action) will be called to find the
+    /// action to perform.
     async fn cleanup(
         &self,
         client: Client,
         resource: &Self::Resource,
     ) -> Result<Option<Action>, Self::Error>;
 
-    async fn reconcile(
+    /// This method is called when a call to [`apply`](Self::apply) or
+    /// [`cleanup`](Self::cleanup) returns `Ok(None)`. It should return the
+    /// default [`Action`] to perform. The default implementation will
+    /// requeue the event at a random time between 40 and 60 minutes in the
+    /// future.
+    fn success_action(&self, resource: &Self::Resource) -> Action {
+        // use a better name for the parameter name in the docs
+        let _resource = resource;
+
+        Action::requeue(Duration::from_secs(thread_rng().gen_range(2400..3600)))
+    }
+
+    /// This method is called when a call to [`apply`](Self::apply) or
+    /// [`cleanup`](Self::cleanup) returns `Err`. It should return the
+    /// default [`Action`] to perform. The error returned will be passed in
+    /// here, as well as a count of how many consecutive errors have happened
+    /// for this resource, to allow for an exponential backoff strategy. The
+    /// default implementation uses exponential backoff with a max of 256
+    /// seconds and some added randomization to avoid thundering herds.
+    fn error_action(
+        self: Arc<Self>,
+        resource: Arc<Self::Resource>,
+        err: &kube_runtime::finalizer::Error<Self::Error>,
+        consecutive_errors: u32,
+    ) -> Action {
+        // use a better name for the parameter name in the docs
+        let _resource = resource;
+        let _err = err;
+
+        let seconds = 2u64.pow(consecutive_errors.min(7) + 1);
+        Action::requeue(Duration::from_millis(
+            thread_rng().gen_range((seconds * 500)..(seconds * 1000)),
+        ))
+    }
+
+    #[doc(hidden)]
+    async fn _reconcile(
         self: Arc<Self>,
         client: Client,
         api: Api<Self::Resource>,
@@ -73,7 +313,11 @@ pub trait Context {
                 let action = match event {
                     Event::Apply(resource) => {
                         let action = self.apply(client, &resource).await?;
-                        self.on_success(&resource, action)
+                        if let Some(action) = action {
+                            action
+                        } else {
+                            self.success_action(&resource)
+                        }
                     }
                     Event::Cleanup(resource) => self
                         .cleanup(client, &resource)
@@ -99,174 +343,5 @@ pub trait Context {
             );
         }
         res
-    }
-
-    fn on_success(&self, _resource: &Self::Resource, action: Option<Action>) -> Action {
-        action.unwrap_or_else(|| {
-            Action::requeue(Duration::from_secs(thread_rng().gen_range(2400..3600)))
-        })
-    }
-    fn on_error(
-        self: Arc<Self>,
-        _resource: Arc<Self::Resource>,
-        _err: &kube_runtime::finalizer::Error<Self::Error>,
-        consecutive_errors: u32,
-    ) -> Action {
-        let seconds = 2u64.pow(consecutive_errors.min(7) + 1);
-        Action::requeue(Duration::from_millis(
-            thread_rng().gen_range((seconds * 500)..(seconds * 1000)),
-        ))
-    }
-}
-
-pub struct Controller<Ctx: Context>
-where
-    Ctx: Send + Sync + 'static,
-    Ctx::Error: Send + Sync + 'static,
-    Ctx::Resource: Send + Sync + 'static,
-    Ctx::Resource: Clone + std::fmt::Debug + serde::Serialize,
-    for<'de> Ctx::Resource: serde::Deserialize<'de>,
-    <Ctx::Resource as Resource>::DynamicType:
-        Eq + Clone + std::hash::Hash + std::default::Default + std::fmt::Debug + std::marker::Unpin,
-{
-    client: kube::Client,
-    make_api: Box<dyn Fn(&Ctx::Resource) -> Api<Ctx::Resource> + Sync + Send + 'static>,
-    controller: kube_runtime::controller::Controller<Ctx::Resource>,
-    context: Ctx,
-}
-
-impl<Ctx: Context> Controller<Ctx>
-where
-    Ctx: Send + Sync + 'static,
-    Ctx::Error: Send + Sync + 'static,
-    Ctx::Resource: Send + Sync + 'static,
-    Ctx::Resource: Clone + std::fmt::Debug + serde::Serialize,
-    for<'de> Ctx::Resource: serde::Deserialize<'de>,
-    <Ctx::Resource as Resource>::DynamicType:
-        Eq + Clone + std::hash::Hash + std::default::Default + std::fmt::Debug + std::marker::Unpin,
-{
-    pub fn namespaced(namespace: &str, client: Client, lp: ListParams, context: Ctx) -> Self
-    where
-        Ctx::Resource: Resource<Scope = NamespaceResourceScope>,
-    {
-        let make_api = {
-            let client = client.clone();
-            Box::new(move |resource: &Ctx::Resource| {
-                Api::<Ctx::Resource>::namespaced(client.clone(), &resource.namespace().unwrap())
-            })
-        };
-        let controller = kube_runtime::controller::Controller::new(
-            Api::<Ctx::Resource>::namespaced(client.clone(), namespace),
-            lp,
-        );
-        Self {
-            client,
-            make_api,
-            controller,
-            context,
-        }
-    }
-
-    pub fn namespaced_all(client: Client, lp: ListParams, context: Ctx) -> Self
-    where
-        Ctx::Resource: Resource<Scope = NamespaceResourceScope>,
-    {
-        let make_api = {
-            let client = client.clone();
-            Box::new(move |resource: &Ctx::Resource| {
-                Api::<Ctx::Resource>::namespaced(client.clone(), &resource.namespace().unwrap())
-            })
-        };
-        let controller = kube_runtime::controller::Controller::new(
-            Api::<Ctx::Resource>::all(client.clone()),
-            lp,
-        );
-        Self {
-            client,
-            make_api,
-            controller,
-            context,
-        }
-    }
-
-    pub fn cluster(client: Client, lp: ListParams, context: Ctx) -> Self
-    where
-        Ctx::Resource: Resource<Scope = ClusterResourceScope>,
-    {
-        let make_api = {
-            let client = client.clone();
-            Box::new(move |_: &Ctx::Resource| Api::<Ctx::Resource>::all(client.clone()))
-        };
-        let controller = kube_runtime::controller::Controller::new(
-            Api::<Ctx::Resource>::all(client.clone()),
-            lp,
-        );
-        Self {
-            client,
-            make_api,
-            controller,
-            context,
-        }
-    }
-
-    pub async fn run(self) {
-        let Self {
-            client,
-            make_api,
-            controller,
-            context,
-        } = self;
-        let backoffs = Arc::new(Mutex::new(BTreeMap::new()));
-        let backoffs = &backoffs;
-        controller
-            .run(
-                |resource, context| {
-                    let uid = resource.uid().unwrap();
-                    let backoffs = Arc::clone(backoffs);
-                    context
-                        .reconcile(client.clone(), make_api(&resource), resource)
-                        .inspect(move |result| {
-                            if result.is_ok() {
-                                backoffs.lock().unwrap().remove(&uid);
-                            }
-                        })
-                },
-                |resource, err, context| {
-                    let consecutive_errors = {
-                        let uid = resource.uid().unwrap();
-                        let mut backoffs = backoffs.lock().unwrap();
-                        let consecutive_errors: u32 =
-                            backoffs.get(&uid).copied().unwrap_or_default();
-                        backoffs.insert(uid, consecutive_errors.saturating_add(1));
-                        consecutive_errors
-                    };
-                    context.on_error(resource, err, consecutive_errors)
-                },
-                Arc::new(context),
-            )
-            .for_each(|reconciliation_result| async move {
-                let dynamic_type = Default::default();
-                let kind = Ctx::Resource::kind(&dynamic_type).into_owned();
-                match reconciliation_result {
-                    Ok(resource) => {
-                        event!(
-                            Level::INFO,
-                            resource_name = %resource.0.name,
-                            controller = Ctx::FINALIZER_NAME,
-                            "{} reconciliation successful.",
-                            kind
-                        );
-                    }
-                    Err(err) => event!(
-                        Level::ERROR,
-                        err = %err,
-                        source = err.source(),
-                        controller = Ctx::FINALIZER_NAME,
-                        "{} reconciliation error.",
-                        kind
-                    ),
-                }
-            })
-            .await
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -60,6 +60,112 @@
 #![warn(clippy::disallowed_types)]
 #![warn(clippy::from_over_into)]
 
+//! This crate implements a lightweight framework around
+//! [`kube_runtime::Controller`] which provides a simpler interface for common
+//! controller patterns. To use it, you define the data that your controller is
+//! going to operate over, and implement the `Context` trait on that struct:
+//!
+//! ```no_run
+//! # use std::collections::BTreeSet;
+//! # use std::sync::{Arc, Mutex};
+//! # use k8s_openapi::api::core::v1::Pod;
+//! # use kube::{Client, Resource};
+//! # use kube_runtime::controller::Action;
+//! #[derive(Default, Clone)]
+//! struct PodCounter {
+//!     pods: Arc<Mutex<BTreeSet<String>>>,
+//! }
+//!
+//! impl PodCounter {
+//!     fn pod_count(&self) -> usize {
+//!         let mut pods = self.pods.lock().unwrap();
+//!         pods.len()
+//!     }
+//! }
+//!
+//! #[async_trait::async_trait]
+//! impl kube_controller::Context for PodCounter {
+//!     type Resource = Pod;
+//!     type Error = kube::Error;
+//!
+//!     const FINALIZER_NAME: &'static str = "example.com/pod-counter";
+//!
+//!     async fn apply(
+//!         &self,
+//!         client: Client,
+//!         pod: &Self::Resource,
+//!     ) -> Result<Option<Action>, Self::Error> {
+//!         let mut pods = self.pods.lock().unwrap();
+//!         pods.insert(pod.meta().uid.as_ref().unwrap().clone());
+//!         Ok(None)
+//!     }
+//!
+//!     async fn cleanup(
+//!         &self,
+//!         client: Client,
+//!         pod: &Self::Resource,
+//!     ) -> Result<Option<Action>, Self::Error> {
+//!         let mut pods = self.pods.lock().unwrap();
+//!         pods.remove(pod.meta().uid.as_ref().unwrap());
+//!         Ok(None)
+//!     }
+//! }
+//! ```
+//!
+//! Then you can run it against your Kubernetes cluster by creating a
+//! [`Controller`]:
+//!
+//! ```no_run
+//! # use std::collections::BTreeSet;
+//! # use std::sync::{Arc, Mutex};
+//! # use std::thread::sleep;
+//! # use std::time::Duration;
+//! # use k8s_openapi::api::core::v1::Pod;
+//! # use kube::{Config, Client};
+//! # use kube::api::ListParams;
+//! # use kube_runtime::controller::Action;
+//! # use tokio::task;
+//! # #[derive(Default, Clone)]
+//! # struct PodCounter {
+//! #     pods: Arc<Mutex<BTreeSet<String>>>,
+//! # }
+//! # impl PodCounter {
+//! #     fn pod_count(&self) -> usize { todo!() }
+//! # }
+//! # #[async_trait::async_trait]
+//! # impl kube_controller::Context for PodCounter {
+//! #     type Resource = Pod;
+//! #     type Error = kube::Error;
+//! #     const FINALIZER_NAME: &'static str = "example.com/pod-counter";
+//! #     async fn apply(
+//! #         &self,
+//! #         client: Client,
+//! #         pod: &Self::Resource,
+//! #     ) -> Result<Option<Action>, Self::Error> { todo!() }
+//! #     async fn cleanup(
+//! #         &self,
+//! #         client: Client,
+//! #         pod: &Self::Resource,
+//! #     ) -> Result<Option<Action>, Self::Error> { todo!() }
+//! # }
+//! # async fn foo() {
+//! let kube_config = Config::infer().await.unwrap();
+//! let kube_client = Client::try_from(kube_config).unwrap();
+//! let context = PodCounter::default();
+//! let controller = kube_controller::Controller::namespaced_all(
+//!     kube_client,
+//!     context.clone(),
+//!     ListParams::default(),
+//! );
+//! task::spawn(controller.run());
+//!
+//! loop {
+//!     println!("{} pods running", context.pod_count());
+//!     sleep(Duration::from_secs(1));
+//! }
+//! # }
+//! ```
+
 mod controller;
 
 pub use controller::{Context, Controller};
